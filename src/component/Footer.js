9779186ed6d1f2027b8252d08e140030e0ea9192
conghuyen tahuynh <tahuynhconghuyen352000@gmail.com>
2024-09-09 21:28:26 +0700

    update footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="footer">
       <div className="top">
         <div>
-          <h1>TravelTour</h1>
+          <h1>BeautyBeach</h1>
           <p>Choose your favourite destination</p>
         </div>
         <div>
